ed95a36bb5a66f3978d4706037e75d94c128ff3f
Daniel Barrios <danielbarrios76@gmail.com>
2018-10-23 22:10:28 -0300

    [Issue02] full CRUD for MEMO end point

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -2,8 +2,7 @@ class Memo < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
 
-
-  def comply
-    update_atributes(complied: true)
+  def comply!
+    update_attributes(complied: true)
   end
 end
